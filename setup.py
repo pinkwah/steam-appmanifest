a278c466a7597758332f8120eacb045d92731db8
Dylan Grafmyre <thorsummoner@live.com>
2016-10-24 20:55:46 -0700

    python2 support

--- setup.py
+++ setup.py
@@ -19,6 +19,9 @@ setuptools.setup(
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
 
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
